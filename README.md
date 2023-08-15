9422b0199ac83a18e22cb38f3e41bf46fe65ab4a
Eme Hache <luisalfonsodsg@outlook.com>
2023-08-15 13:35:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cinemapedia
+
+
+# Dev
+
+1. Copy .env.template and rename it as .env
+2. Change the env variable "The MovieDB"
---
+++ lib/presentation/screens/movies/home_movies_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeMoviesScreen extends StatelessWidget {
+  static const String name = 'home-screen';
+
+  const HomeMoviesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
